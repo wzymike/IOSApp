13ca992df9875d8705760822fae9b79be740439e
Criss <wuzheyun@126.com>
2018-11-22 00:51:45 +0800

    create branch dev

---
+++ IOSApp/MainViewController.swift
@@ -0,0 +1,34 @@
+//
+//  MainViewController.swift
+//  IOSApp
+//
+//  Created by wzy on 2018/11/6.
+//  Copyright © 2018年 Criss. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewWillAppear(_ animated: Bool) {
+        navigationController?.setNavigationBarHidden(true, animated: true)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ IOSApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IOSApp
+//
+//  Created by 吴哲耘 on 2018/10/16.
+//  Copyright © 2018年 Criss. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewWillAppear(_ animated: Bool) {
+        navigationController?.setNavigationBarHidden(false, animated: true)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
